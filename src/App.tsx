534350adbb98ecc5f31fcf8450e850336cf0fc08
ragul <141637129+ragultv@users.noreply.github.com>
2025-04-29 15:16:52 +0530

    Update profile page logout handle

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
           element={
             <ProtectedRoute>
               <Layout>
-                <Profile />
+                <Profile onLogout={() => setIsLoggedIn(false)} />
               </Layout>
             </ProtectedRoute>
           }
